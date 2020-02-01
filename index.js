67438d79015978d96b5a98a4d9943fa8a8259c6c
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2020-02-01 13:47:41 +0000

    Updated method of rendering character expressions (in-progress)

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 window.onload = function() {
 
-    var arrAiko = ["aikoPlain", "aikoBlush1", "aikoBlush2", "aikoBlush3"];
-
-    var x = 0;
-
     aiko.onclick = function() {
-        x = x + 1;
-        if (x == arrAiko.length) x = 0;
-        aiko.style.background = (`url("images/character/${arrAiko[x]}.png") center/contain no-repeat`);
-        console.log(arrAiko[x]);   
+        changeExpression('plain2');  
+    }
+
+    const changeExpression = expression => {
+        var x = 48;
+        switch(expression) {
+            case 'plain1': 
+                x = '3rem';
+                break;
+            case 'plain2':
+                x = -248;
+                break;
+            case 'smile1':
+                x = '-34rem';
+                break;
+            case 'smile2':
+                x = ''
+            case 'cry1':
+                x = '-163.5rem';
+        }
+        aiko.style.backgroundPosition = x + "px";
     }
 }
 
